4578a954a4e834a64dac1944d55444837011044c
oureveryday <oureveryday@163.com>
2025-02-26 15:48:23 +0800

    311860

---
+++ 311860.lua
@@ -0,0 +1,5 @@
+addappid(311860)
+addappid(228982)
+setManifestid(228982,"6413394087650432851")
+addappid(311861,0,"fc76e4660fbac88fa2baa81ee53bf4c3f4a6f2bbea74bd42cdbc1f0b2a850afb")
+setManifestid(311861,"3087064667638749873")
