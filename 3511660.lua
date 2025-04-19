ad2feca85a45f1bfe160394e7530a3031083c474
oureveryday <oureveryday@163.com>
2025-04-20 00:46:53 +0800

    Update 3511660 manifest files

---
+++ 3511660.lua
@@ -0,0 +1,6 @@
+addappid(3511660)
+addappid(3511661)
+addappid(3511662,0,"afd17ae5075ad9b16ab80aefbadd87b22980318f6fb7c238aed2f75b7aa150a8")
+setManifestid(3511662,"7004906032929400705")
+addappid(3511663)
+addappid(3511664)
