8b0df768f2b09e4228e087faa965ca9a694261fd
oureveryday <oureveryday@163.com>
2025-04-19 23:57:47 +0800

    Update 368650 manifest files

--- 368650.lua
+++ 368650.lua
@@ -1,3 +1,5 @@
 addappid(368650)
 addappid(368651,0,"e5cbc89acc7aeccb4b7f28ada24f43c323712ef6894daaef49b8c8d00c92f2d2")
 setManifestid(368651,"2094242094813114602")
+addappid(368652)
+addappid(368653)
