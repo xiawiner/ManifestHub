6e0b01473a8b24bbef1a77529fa6e83d13b3bd12
oureveryday <oureveryday@163.com>
2025-04-20 00:41:04 +0800

    Update 604490 manifest files

--- 604490.lua
+++ 604490.lua
@@ -1,3 +1,5 @@
 addappid(604490)
 addappid(604491,0,"fdc9c4fcb5836df3321554af6211fab2efef45c6e3eb16cdb78005ec03776ca5")
 setManifestid(604491,"4931961821736812084")
+addappid(604492)
+addappid(604493)
