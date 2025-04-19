43df83456e045541d12e6b689dc7dff197ffb318
oureveryday <oureveryday@163.com>
2025-04-20 00:27:04 +0800

    Update 660920 manifest files

---
+++ 660920.lua
@@ -0,0 +1,7 @@
+addappid(660920)
+addappid(228986)
+addappid(228990)
+setManifestid(228990,"1829726630299308803")
+addappid(229005)
+addappid(229030)
+addappid(660921,0,"b9a2ee682e0dbf3b53e1368e2fef571a42397ff0000425215de07eac595508a1")
