08584800b2714b1abe1c719a2517a159830cb649
oureveryday <oureveryday@163.com>
2025-04-20 00:09:40 +0800

    Update 882200 manifest files

---
+++ 882200.lua
@@ -0,0 +1,6 @@
+addappid(882200)
+addappid(228986)
+addappid(228990)
+setManifestid(228990,"1829726630299308803")
+addappid(229000)
+addappid(882201,0,"7a173fbfbcdae4ad51c91459c21ced75843b5129b6219b52e33a8d83500efb73")
