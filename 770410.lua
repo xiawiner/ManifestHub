3c4f5cf7a80f2b71bd70ba764bfe06cc6b5a5166
oureveryday <oureveryday@163.com>
2025-04-20 00:39:41 +0800

    Update 770410 manifest files

--- 770410.lua
+++ 770410.lua
@@ -1,3 +1,6 @@
 addappid(770410)
 addappid(770411,0,"ba0e52f28a9caf87f43acb45eb981e800212703a433c00daf88b77b2ff7cd933")
 setManifestid(770411,"4273103566554260050")
+addappid(770412)
+addappid(770413)
+addappid(770414)
