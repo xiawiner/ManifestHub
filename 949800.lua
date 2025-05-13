5184acb9c66ec432eb5f38d174e151337d0d6db3
oureveryday <oureveryday@163.com>
2025-05-14 01:42:26 +0800

    Update 949800 manifest files

--- 949800.lua
+++ 949800.lua
@@ -1,3 +1,5 @@
 addappid(949800)
 addappid(949801,0,"1211190be26de4e2ac8a46f88c7eab7f1149497a67e3d41894dca2cae1be2fe0")
 setManifestid(949801,"6294709087751103870")
+addappid(949802)
+addappid(949803)
