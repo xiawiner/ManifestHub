d9f35575f844be593854d17b5bb713dabf1d9a1e
oureveryday <oureveryday@163.com>
2025-05-14 00:34:08 +0800

    Update 501220 manifest files

--- 501220.lua
+++ 501220.lua
@@ -1,3 +1,6 @@
 addappid(501220)
 addappid(501221,0,"616a7c2f8c1a39fff8268a684aa7bb6cd0c371000f4fa89f6f96ad1346bba7e4")
 setManifestid(501221,"8561490978189762661")
+addappid(501222)
+addappid(501223)
+addappid(501224)
