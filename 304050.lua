537fc359f0a0d83be2f7ad77f3a8a8de359c5d13
oureveryday <oureveryday@163.com>
2025-05-14 00:34:31 +0800

    Update 304050 manifest files

---
+++ 304050.lua
@@ -0,0 +1,7 @@
+addappid(304050)
+addappid(845241,0,"9cbac5fd5355370c3b656df7c5b596aa9bb794cb3cfc2bdc56e9581580a102f2")
+setManifestid(845241,"7675389946101989167")
+addappid(304053)
+addappid(304051)
+addappid(462050)
+addappid(304052)
