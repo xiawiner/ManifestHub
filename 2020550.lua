4d0f8cebc083504d7f4fca622655bff279d0785b
oureveryday <oureveryday@163.com>
2025-05-14 01:23:30 +0800

    Update 2020550 manifest files

--- 2020550.lua
+++ 2020550.lua
@@ -1,4 +1,5 @@
 addappid(2020550)
+addappid(228989)
 addappid(228990)
 setManifestid(228990,"1829726630299308803")
 addappid(2020551,0,"0c094fba007baecaa1c2bb0f35f8819a2abcae6cd2e75bc9407011453345e4fa")
