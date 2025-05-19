372b3be19e2400b1be5341786848d55be667a049
oureveryday <oureveryday@163.com>
2025-05-19 14:01:04 +0800

    Update 1038620 manifest files

--- 1038620.lua
+++ 1038620.lua
@@ -1,3 +1,5 @@
 addappid(1038620)
 addappid(1038621,0,"4588b7efacca1252df5ccc4cedf1bc1dec480ebe5a9fe5a79ab1fc7ca51cbc53")
 setManifestid(1038621,"5169762982327172988")
+addappid(1038622)
+addappid(1038623)
