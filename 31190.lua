f0dfd833a6ebd7e70d691360e39d028e08248eed
oureveryday <oureveryday@163.com>
2025-05-19 14:50:52 +0800

    Update 31190 manifest files

--- 31190.lua
+++ 31190.lua
@@ -1,3 +1,6 @@
 addappid(31190)
+addappid(31192)
 addappid(31191,0,"cef4b106b8b784b8aef53d287b6cfd4dee4fec7a6f1c9db2d2deafa58c7db360")
 setManifestid(31191,"3101865445816834694")
+addappid(31193)
+addappid(31194)
