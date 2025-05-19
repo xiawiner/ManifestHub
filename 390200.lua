682062034435b08b8d954b42ee8570a5b8731e43
oureveryday <oureveryday@163.com>
2025-05-19 14:34:06 +0800

    Update 390200 manifest files

--- 390200.lua
+++ 390200.lua
@@ -1,3 +1,5 @@
 addappid(390200)
 addappid(390201,0,"ca9fde3ed43b6fac0a94d7679d7b651dbd50e8bc3f5d577db7e0d12f21b90820")
 setManifestid(390201,"5486178295507705703")
+addappid(390202)
+addappid(390203)
