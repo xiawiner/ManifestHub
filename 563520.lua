8395a310e0c4b2dc2f9ce837e61cbee3df98286d
oureveryday <oureveryday@163.com>
2025-05-19 14:48:44 +0800

    Update 563520 manifest files

--- 563520.lua
+++ 563520.lua
@@ -1,3 +1,7 @@
 addappid(563520)
+addappid(563521)
 addappid(563522,0,"5f2569f18e9595838d8b5bac0e39b54f2ef68f192eb8cade2e21c3f3b9aee3cc")
 setManifestid(563522,"2146563610757480394")
+addappid(563523)
+addappid(581831)
+addappid(581830)
