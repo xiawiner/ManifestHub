81869bcc251277031b1fc28a26d9874e9ed7f74e
oureveryday <oureveryday@163.com>
2025-05-24 19:20:06 +0800

    Update 407000 manifest files

--- 407000.lua
+++ 407000.lua
@@ -1,3 +1,8 @@
 addappid(407000)
+addappid(407001)
 addappid(407002,0,"7b30e4355e223ce379b87ab348e4c7dad571f7fad0a7f1eb1750c08d3230a455")
 setManifestid(407002,"6452283665289243832")
+addappid(407003)
+addappid(407004)
+addappid(407005)
+addappid(407006)
