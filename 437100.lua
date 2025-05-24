4ea33e1224db6b791fade2cf681acfbd5d2d633d
oureveryday <oureveryday@163.com>
2025-05-24 15:00:58 +0800

    Update 437100 manifest files

--- 437100.lua
+++ 437100.lua
@@ -1,3 +1,5 @@
 addappid(437100)
 addappid(437101,0,"3f6ec807031c38fae0c3eb1976a4985fb6f368d3795cea4f9ffdcf584a54b8f1")
 setManifestid(437101,"5731242680489723734")
+addappid(437102)
+addappid(437103)
