3eb243ee466d3bc643fccdb1906793538d67fb08
oureveryday <oureveryday@163.com>
2025-05-24 16:35:49 +0800

    Update 618360 manifest files

--- 618360.lua
+++ 618360.lua
@@ -1,3 +1,6 @@
 addappid(618360)
+addappid(618361)
 addappid(618362,0,"42b5a11fb6dff41711d7d12ecf11627ddb144d2c21a6c2efee2a7dd309f1ada8")
 setManifestid(618362,"5434018289625677211")
+addappid(618363)
+addappid(622130)
