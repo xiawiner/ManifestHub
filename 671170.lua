57fb1fccfe2b02c859ffe52a0af0243719f37a3e
oureveryday <oureveryday@163.com>
2025-05-24 16:18:52 +0800

    Update 671170 manifest files

---
+++ 671170.lua
@@ -0,0 +1,6 @@
+addappid(671170)
+addappid(671171)
+addappid(671172,0,"a8abfac7d9a0b90dbb92b33c84cccd9c89b7b43e821d77e20800f729fbd68bf4")
+setManifestid(671172,"5943794059154501384")
+addappid(671173)
+addappid(671174)
