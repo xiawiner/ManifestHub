ee78880a7bd4c71cceea6cbc039d3fab4525b084
oureveryday <oureveryday@163.com>
2025-05-24 16:31:07 +0800

    Update 314560 manifest files

---
+++ 314560.lua
@@ -0,0 +1,7 @@
+addappid(314560)
+addappid(314561,0,"1f6bc42e925663cd834be5447e3c6dfc2d8bb0ffba1ac575fed30c6e91bc7209")
+setManifestid(314561,"328188274423149819")
+addappid(314562)
+addappid(314563)
+addappid(378650)
+addappid(389960)
