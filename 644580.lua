1ee901929f4aef869ea13dbb93482f94bdfb25ec
oureveryday <oureveryday@163.com>
2025-05-24 18:55:53 +0800

    Update 644580 manifest files

--- 644580.lua
+++ 644580.lua
@@ -1,3 +1,5 @@
 addappid(644580)
 addappid(644581,0,"dbc2b5fde164f8992ec6ed4ab84296f04310c4bb4ca63fc9f77ea39bd71a506c")
 setManifestid(644581,"2243376406672934551")
+addappid(644582)
+addappid(644583)
