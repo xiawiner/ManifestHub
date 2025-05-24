19cb900ff3bcdc5b01fa8c6123e834c239592a5d
oureveryday <oureveryday@163.com>
2025-05-24 16:22:37 +0800

    Update 406730 manifest files

--- 406730.lua
+++ 406730.lua
@@ -1,3 +1,5 @@
 addappid(406730)
 addappid(406731,0,"6efafbd586f516dc8ff7c1ac45f5bda0336cceb12f6411e6c90f8179eb329e20")
 setManifestid(406731,"6905959842585818489")
+addappid(406732)
+addappid(406733)
