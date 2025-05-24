ec42c32aea5fafa218c71f9241b9782b261e7c79
oureveryday <oureveryday@163.com>
2025-05-24 18:10:31 +0800

    Update 733050 manifest files

--- 733050.lua
+++ 733050.lua
@@ -1,3 +1,5 @@
 addappid(733050)
 addappid(733051,0,"9d7710ddc1da2c4079d49a7edecaefc631d4804bfa57ec1dbc9b3abcb0ccebcf")
 setManifestid(733051,"4920676495861867353")
+addappid(733052)
+addappid(733053)
