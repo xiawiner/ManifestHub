b05792ab4bfaaa4729e29d66ae485fc86b0cfe77
oureveryday <oureveryday@163.com>
2025-05-24 19:57:00 +0800

    Update 766000 manifest files

--- 766000.lua
+++ 766000.lua
@@ -1,3 +1,6 @@
 addappid(766000)
 addappid(766001,0,"6bbbf4bc43c8d6eaa225d820b11c9a1b6109dcb0e8c839995c88b97aeac8cd0c")
 setManifestid(766001,"5352279832211458073")
+addappid(766004)
+addappid(766002)
+addappid(766003)
