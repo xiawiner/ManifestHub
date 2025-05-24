3370b2d4999a99d6e5492c394d0953b3d5395186
oureveryday <oureveryday@163.com>
2025-05-24 16:07:59 +0800

    Update 2892470 manifest files

---
+++ 2892470.lua
@@ -0,0 +1,6 @@
+addappid(2892470)
+addappid(228989)
+setManifestid(228989,"550968249685141759")
+addappid(228990)
+setManifestid(228990,"1829726630299308803")
+addappid(2892471,0,"a5e0a1ad74c6be9ec5b49fd48cbf19a75d26f4bc897edaec9e46d29bcffa9ef2")
