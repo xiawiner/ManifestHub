0457b409a08430a4112107372ff7a35eda692f47
oureveryday <oureveryday@163.com>
2025-06-25 18:41:49 +0800

    Update 397740 manifest files

---
+++ 397740.lua
@@ -0,0 +1,4 @@
+addappid(397740)
+addappid(397741,0,"ebbcfcee8fe8358c430edbcf2f68f0feefeaaeefaf266acb9d28fa125049d851")
+setManifestid(397741,"1020712750966146550")
+addappid(397742,0,"2281ebe2d4eca8c1ba5721aa60349be3faa85e3db67114d2bb72deb927450b3b")
